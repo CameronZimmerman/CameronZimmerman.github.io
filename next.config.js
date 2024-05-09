0608f34892521a2b2799f26e0c104a30925a5338
Cameron Zimmerman <cameronzimmerman@Camerons-MacBook-Pro.local>
2024-05-08 20:09:53 -0400

    update next config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: "/portfolio",
+  output: "standalone",
   reactStrictMode: true,
   swcMinify: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
